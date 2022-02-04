7b91946217cb6d88c28de44e7d39b055a662d1ed
david <david@DESKTOP-P99GIPF>
2022-02-04 18:03:52 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa/Entities/Veiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbastecerVeiculo.Entities
+{
+    internal class Veiculo
+    {
+        public Veiculo(string Nome, string Marca, int capacidadeDoTanque, int litrosNoTanque)
+        {
+            this.Nome = Nome;
+            this.Marca = Marca;
+            this.capacidadeDoTanque = capacidadeDoTanque;
+            this.litrosNoTanque = litrosNoTanque;
+        }
+
+        public string Nome;
+
+        public string Marca;
+
+        public int capacidadeDoTanque;
+
+        public int litrosNoTanque;
+
+    }
+}
---
+++ Pessoa/Program.cs
@@ -0,0 +1,10 @@
+﻿using AbastecerVeiculo.Entities;
+
+Console.Write("Digite o nome do veiculo: ");
+string Nome = Console.ReadLine();
+Console.Write("Digite a marca do veiculo: ");
+string Marca = Console.ReadLine();
+Console.Write("Digite a capacidade do tanque: ");
+int capacidadeDoTanque = int.Parse(Console.ReadLine());
+Console.Write("Digite quantos litros tem no tanque: ");
+int litrosNoTanque = int.Parse(Console.ReadLine());
